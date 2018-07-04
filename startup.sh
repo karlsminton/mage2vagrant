10d3313527fc172e71869559a5c1cf391e2ce10e
Karl Minton <karl.minton@readingroom.com>
2018-07-04 12:19:24 +0100

    Updated to php 7.1

--- startup.sh
+++ startup.sh
@@ -4,21 +4,27 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get upgrade -y
+
+apt-get install python-software-properties -y
+add-apt-repository ppa:ondrej/php -y
+sudo apt-get update -y
 apt-get install mysql-server-5.7 mysql-client-5.7 -y
 apt-get install apache2 -y
-apt-get install php -y
+
+apt-get install php7.1 -y
 apt-get install libapache2-mod-php -y
 service apache2 restart
-apt-get install php-xml -y
+apt-get install php7.1-xml -y
 apt-get install mysql-server-5.6 -y
-apt-get install php-gd -y
-apt-get install php-curl -y
-apt-get install php-mcrypt -y
-apt-get install php-intl -y
-apt-get install php-mbstring -y
-apt-get install php-zip -y
-apt-get install php-mysql -y
-apt-get install php-soap -y
+
+apt-get install php7.1-gd -y
+apt-get install php7.1-curl -y
+apt-get install php7.1-mcrypt -y
+apt-get install php7.1-intl -y
+apt-get install php7.1-mbstring -y
+apt-get install php7.1-zip -y
+apt-get install php7.1-mysql -y
+apt-get install php7.1-soap -y
 sudo ln -fs /vagrant/public_html/ /var/www/site
 
 FILE="/etc/apache2/sites-available/default.conf"
@@ -39,6 +45,9 @@ a2ensite default.conf
 service apache2 restart
 mkdir /vagrant/public_html
 mkdir /var/www/site
+
+rm /usr/bin/php
+ln -sf /usr/bin/php7.1 /usr/bin/php
 
 echo "##############################"
 echo "### Add to your host machine"
